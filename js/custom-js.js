a3fe4f76de6acee30e455f82b6fbf03a6bdcbf72
sumitshevade <sumitshevade@gmail.com>
2020-08-07 19:22:07 +0530

    Many changes.

--- js/custom-js.js
+++ js/custom-js.js
@@ -12,4 +12,22 @@ $(document).ready(function () {
     $("#saveContact").click(function () {
         $("#contactForm").toggle();
     });
+
+    // START: Header detail master page
+    
+    $("#detailForm").hide();
+
+    $("#addDetail").click(function () {
+        $("#detailForm").toggle();
+    });
+
+    $("#cancelDetail").click(function () {
+        $("#detailForm").toggle();
+    });
+
+    $("#saveDetail").click(function () {
+        $("#detailForm").toggle();
+    });
+    
+    // END: Header detail master page
 });
